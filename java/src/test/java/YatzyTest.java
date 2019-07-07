47ed82171af036aca589b364bbc08c3fbee21457
MEDDAH Julien <julien@bloodstone.software>
2019-07-08 00:29:19 +0200

    Badly worded test method names

--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -28,7 +28,7 @@ public class YatzyTest {
             "1, 2, 1, 4, 5, 2",
             "6, 2, 2, 4, 5, 0",
             "1, 2, 1, 1, 1, 4"})
-    public void should_ones_score_occurrences_of_one(int d1, int d2, int d3, int d4, int d5, int expected) {
+    public void should_ones_score_sum_of_ones(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertThat(new Yatzy(d1, d2, d3, d4, d5).ones()).isEqualTo(expected);
     }
 
@@ -36,7 +36,7 @@ public class YatzyTest {
     @CsvSource(value = {
             "1, 2, 3, 2, 6, 4",
             "2, 2, 2, 2, 2, 10"})
-    public void should_twos_score_occurrences_of_two_doubled(int d1, int d2, int d3, int d4, int d5, int expected) {
+    public void should_twos_score_sum_of_twos(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertThat(new Yatzy(d1, d2, d3, d4, d5).twos()).isEqualTo(expected);
     }
 
@@ -44,7 +44,7 @@ public class YatzyTest {
     @CsvSource(value = {
             "1, 2, 3, 2, 3, 6",
             "2, 3, 3, 3, 3, 12"})
-    public void should_threes_score_occurrences_of_three_tripled(int d1, int d2, int d3, int d4, int d5, int expected) {
+    public void should_threes_score_sum_of_threes(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertThat(new Yatzy(d1, d2, d3, d4, d5).threes()).isEqualTo(expected);
     }
 
@@ -53,7 +53,7 @@ public class YatzyTest {
             "4, 4, 4, 5, 5, 12",
             "4, 4, 5, 5, 5, 8",
             "4, 5, 5, 5, 5, 4"})
-    public void should_fours_score_occurrences_of_four_quadrupled(int d1, int d2, int d3, int d4, int d5, int expected) {
+    public void should_fours_score_sum_of_fours(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertThat(new Yatzy(d1, d2, d3, d4, d5).fours()).isEqualTo(expected);
     }
 
@@ -62,7 +62,7 @@ public class YatzyTest {
             "4, 4, 4, 5, 5, 10",
             "4, 4, 5, 5, 5, 15",
             "4, 5, 5, 5, 5, 20"})
-    public void should_fives_score_occurrences_of_five_quintupled(int d1, int d2, int d3, int d4, int d5, int expected) {
+    public void should_fives_score_sum_of_fives(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertThat(new Yatzy(d1, d2, d3, d4, d5).fives()).isEqualTo(expected);
     }
 
@@ -71,7 +71,7 @@ public class YatzyTest {
             "4, 4, 4, 5, 5, 0",
             "4, 4, 6, 5, 5, 6",
             "6, 5, 6, 6, 5, 18"})
-    public void should_sixes_score_occurrences_of_six_sextupled(int d1, int d2, int d3, int d4, int d5, int expected) {
+    public void should_sixes_score_sum_of_sixes(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertThat(new Yatzy(d1, d2, d3, d4, d5).sixes()).isEqualTo(expected);
     }
 
