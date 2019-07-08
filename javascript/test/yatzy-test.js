9b1a267bc22f0b99a1da6bbaa5cc4879305b20a4
MEDDAH Julien <julien@bloodstone.software>
2019-07-08 17:38:32 +0200

    Coherent test method names

--- javascript/test/yatzy-test.js
+++ javascript/test/yatzy-test.js
@@ -6,14 +6,14 @@ const Yatzy = require('../lib/yatzy');
 
 
 describe('Chance', function () {
-    it('scores sum of all dice', function () {
+    it('should score the sum of all dice', function () {
         assert.equal(15, new Yatzy(2, 3, 4, 5, 1).chance());
         assert.equal(16, new Yatzy(3, 3, 4, 5, 1).chance());
     });
 });
 
 describe('Yatzy', function () {
-    it('scores 50', function () {
+    it('should score 50 when all dice are equal', function () {
         assert.equal(50, new Yatzy(4, 4, 4, 4, 4).yatzy());
         assert.equal(50, new Yatzy(6, 6, 6, 6, 6).yatzy());
         assert.equal(0, new Yatzy(6, 6, 6, 6, 3).yatzy());
@@ -21,7 +21,7 @@ describe('Yatzy', function () {
 });
 
 describe('Ones', function () {
-    it('score the sum of 1s', function () {
+    it('should score the sum of ones', function () {
         assert.equal(1, new Yatzy(1, 2, 3, 4, 5).ones());
         assert.equal(2, new Yatzy(1, 2, 1, 4, 5).ones());
         assert.equal(0, new Yatzy(6, 2, 2, 4, 5).ones());
@@ -30,21 +30,21 @@ describe('Ones', function () {
 });
 
 describe('Twos', function () {
-    it('score the sum of 2s', function () {
+    it('should score the sum of twos', function () {
         assert.equal(4, new Yatzy(1, 2, 3, 2, 6).twos());
         assert.equal(10, new Yatzy(2, 2, 2, 2, 2).twos());
     });
 });
 
 describe('Threes', function () {
-    it('score the sum of 3s', function () {
+    it('should score the sum of threes', function () {
         assert.equal(6, new Yatzy(1, 2, 3, 2, 3).threes());
         assert.equal(12, new Yatzy(2, 3, 3, 3, 3).threes());
     });
 });
 
 describe('Fours', function () {
-    it('score the sum of 4s', function () {
+    it('should score the sum of fours', function () {
         assert.equal(12, new Yatzy(4, 4, 4, 5, 5).fours());
         assert.equal(8, new Yatzy(4, 4, 5, 5, 5).fours());
         assert.equal(4, new Yatzy(4, 5, 5, 5, 5).fours());
@@ -52,7 +52,7 @@ describe('Fours', function () {
 });
 
 describe('Fives', function () {
-    it('score the sum of fives', function () {
+    it('should score the sum of fives', function () {
         assert.equal(10, new Yatzy(4, 4, 4, 5, 5).fives());
         assert.equal(15, new Yatzy(4, 4, 5, 5, 5).fives());
         assert.equal(20, new Yatzy(4, 5, 5, 5, 5).fives());
@@ -60,7 +60,7 @@ describe('Fives', function () {
 });
 
 describe('Sixes', function () {
-    it('score the sum of sixes', function () {
+    it('should score the sum of sixes', function () {
         assert.equal(0, new Yatzy(4, 4, 4, 5, 5).sixes());
         assert.equal(6, new Yatzy(4, 4, 6, 5, 5).sixes());
         assert.equal(18, new Yatzy(6, 5, 6, 6, 5).sixes());
@@ -68,22 +68,22 @@ describe('Sixes', function () {
 });
 
 describe('One pair', function () {
-    it('scores the sum of the highest pair', function () {
+    it('should score the sum of the highest pair', function () {
         assert.equal(6, new Yatzy(3, 4, 3, 5, 6).pair());
         assert.equal(10, new Yatzy(5, 3, 3, 3, 5).pair());
         assert.equal(12, new Yatzy(5, 3, 6, 6, 5).pair());
     });
 });
 
-describe('Two pair', function () {
-    it('scores the sum of the two pairs', function () {
+describe('Two pairs', function () {
+    it('should score the sum of the two pairs', function () {
         assert.equal(16, new Yatzy(3, 3, 5, 4, 5).twoPairs());
         assert.equal(16, new Yatzy(3, 3, 5, 5, 5).twoPairs());
     });
 });
 
 describe('Three of a kind', function () {
-    it('scores the sum of the three of the kind', function () {
+    it('should score the sum of the three of the kind', function () {
         assert.equal(9, new Yatzy(3, 3, 3, 4, 5).threeOfAKind());
         assert.equal(15, new Yatzy(5, 3, 5, 4, 5).threeOfAKind());
         assert.equal(9, new Yatzy(3, 3, 3, 3, 5).threeOfAKind());
@@ -91,7 +91,7 @@ describe('Three of a kind', function () {
 });
 
 describe('Four of a kind', function () {
-    it('scores the sum of the four of the kind', function () {
+    it('should score the sum of the four of the kind', function () {
         assert.equal(12, new Yatzy(3, 3, 3, 3, 5).fourOfAKind());
         assert.equal(20, new Yatzy(5, 5, 5, 4, 5).fourOfAKind());
         assert.equal(12, new Yatzy(3, 3, 3, 3, 3).fourOfAKind());
@@ -99,7 +99,7 @@ describe('Four of a kind', function () {
 });
 
 describe('Small straight', function () {
-    it('scores 15', function () {
+    it('should score 15 when dice form a small straight', function () {
         assert.equal(15, new Yatzy(1, 2, 3, 4, 5).smallStraight());
         assert.equal(15, new Yatzy(2, 3, 4, 5, 1).smallStraight());
         assert.equal(0, new Yatzy(1, 2, 2, 4, 5).smallStraight());
@@ -107,7 +107,7 @@ describe('Small straight', function () {
 });
 
 describe('Large straight', function () {
-    it('scores 20', function () {
+    it('should score 20 when dice form a large straight', function () {
         assert.equal(20, new Yatzy(6, 2, 3, 4, 5).largeStraight());
         assert.equal(20, new Yatzy(2, 3, 4, 5, 6).largeStraight());
         assert.equal(0, new Yatzy(1, 2, 2, 4, 5).largeStraight());
@@ -115,7 +115,7 @@ describe('Large straight', function () {
 });
 
 describe('Full house', function () {
-    it('scores the sum of the full house', function () {
+    it('should score the sum of the full house', function () {
         assert.equal(18, new Yatzy(6, 2, 2, 2, 6).fullHouse());
         assert.equal(0, new Yatzy(2, 3, 4, 5, 6).fullHouse());
     });
